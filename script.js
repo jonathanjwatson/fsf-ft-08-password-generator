bdcc0e53243f78d10719a6f5bdc10a4cbd16033d
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-09-16 21:41:12 -0400

    added generatePassword to resolve error

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function generatePassword() {
+  return "This will be replaced by my generated password.";
 }
 
 // Add event listener to generate button
